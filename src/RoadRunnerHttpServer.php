3d1fbec92bb4967415e63ebb19e752ded0628b4a
liutao <gg.cn.lt@gmail.com>
2023-09-07 16:36:04 +0800

    add runtime

--- src/RoadRunnerHttpServer.php
+++ src/RoadRunnerHttpServer.php
@@ -5,23 +5,21 @@ declare(strict_types=1);
 namespace MicroPHP\RoadRunner;
 
 use JsonException;
-use League\Route\Http\Exception\MethodNotAllowedException;
-use League\Route\Http\Exception\NotFoundException;
-use League\Route\Router;
-use MicroPHP\Framework\Http\Contract\ServerInterface;
-use MicroPHP\Framework\Http\Response;
+use MicroPHP\Framework\Http\Contract\HttpServerInterface;
 use MicroPHP\Framework\Http\ServerRequest;
+use MicroPHP\Framework\Http\Traits\HttpServerTrait;
+use MicroPHP\Framework\Router\Router;
 use Nyholm\Psr7\Factory\Psr17Factory;
 use Spiral\RoadRunner\Http\PSR7Worker;
 use Spiral\RoadRunner\Worker;
 use Throwable;
 
-class RoadRunnerHttpServer implements ServerInterface
+class RoadRunnerHttpServer implements HttpServerInterface
 {
+    use HttpServerTrait;
+
     /**
      * @throws JsonException
-     *
-     * @noinspection PhpRedundantCatchClauseInspection
      */
     public function run(Router $router): void
     {
@@ -34,11 +32,7 @@ class RoadRunnerHttpServer implements ServerInterface
         while ($req = $psr7->waitRequest()) {
             $request = ServerRequest::fromPsr7($req);
             try {
-                $psr7->respond($router->dispatch($request));
-            } catch (NotFoundException $exception) {
-                $psr7->respond(new Response(404, [], $exception->getMessage()));
-            } catch (MethodNotAllowedException $exception) {
-                $psr7->respond(new Response(405, [], $exception->getMessage()));
+                $psr7->respond($this->routeDispatch($router, $request));
             } catch (Throwable $e) {
                 $psr7->getWorker()->error((string) $e);
             }
